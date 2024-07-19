539a5117ed61c26f28a74c4dac3728b37ada5f8f
iammohitsharma-ms <141800566+iammohitsharma-ms@users.noreply.github.com>
2024-07-19 17:35:33 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"      // treat all JS code as a newer version
+
+// alert(2+3)     // we are using nodejs ,not browser
+
+let name = "sharma"            // string
+let age = 55n                //  numbers ,  limit 2 to power 53
+
+// bigint    => for big Numbers , like use in scintific calculation    (type = bigint)
+
+let temp = null              // null is standlon value    (type = object)
+
+let state = undefined       // undefined                  (type = undefined)
+
+let isLoggedIn = true       // true,false                 (type = boolean)
+
+// symbole => symbole for uniqness                         (type = function)
+
+                   
