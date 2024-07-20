854d4d733aa0efe59aeebe72c24011a4dad01bd9
iammohitsharma-ms <141800566+iammohitsharma-ms@users.noreply.github.com>
2024-07-20 05:18:45 +0000

    conversion

--- 01_basics/03_convOpra.js
+++ 01_basics/03_convOpra.js
@@ -35,3 +35,34 @@ let num = 23
 let numINstr = String(num)
 console.log(numINstr);
 console.log(typeof numINstr);
+
+
+/****************************************** OPERATION*****************************/
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// console.log(3+2);
+// console.log(3-2);
+// console.log(3*2);
+// console.log(3/2);
+// console.log(3%2);
+// console.log(3**2);
+
+let str1 = "chaia"
+let str2 = " pani"
+console.log(str1 + str2);
+
+console.log("2" + 3);
+console.log(2 + "3");
+console.log("2" + 3 + 4);
+console.log(2 + 3 + "4");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let gameScore = 100
+++gameScore
+console.log(gameScore);
